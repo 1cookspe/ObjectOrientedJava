face4fd0675f82bce85e85a09ec01dd379a3d6ae
1cookspe <1cookspe@gws4000112.ihdsb.ca>
2015-04-15 11:52:47 -0400

    Triangle

---
+++ src/objectoriented/Triangle.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objectoriented;
+
+/**
+ *
+ * @author 1cookspe
+ */
+public class Triangle {
+    double side1;
+    double side2;
+    double side3;
+    
+    double angle1;
+    double angle2;
+    double angle3;
+
+    public Triangle() {
+    
+    }
+    
+   
+    
+    
+}
